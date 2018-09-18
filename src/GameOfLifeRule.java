dcede1352dd486f58e1aff1fac87a52aaa91683c
Scott McConnell <skm44@duke.edu>
2018-09-18 19:13:37 -0400

    Same process done to GameOfLifeRule

--- src/GameOfLifeRule.java
+++ src/GameOfLifeRule.java
@@ -1,30 +1,32 @@
+import java.util.ArrayList;
+
 /**
  *
- * Extension of RuleInterface to apply rules specifically for Conway's Game Of Life
+ * Extension of main.RuleInterface to apply rules specifically for Conway's Game Of Life
  * Returns nextState for myCell
  *
  * @author Scott McConnell skm44
  **/
-public class GameOfLifeRule implements RuleInterface{
-    public void applyRule(Cell myCell, Cell[] myNeighbors) {
+public class GameOfLifeRule implements RuleInterface {
+    public int applyRule(Cell cell, ArrayList<Cell> myNeighbors) {
         int liveNeighborsCount = 0;
         for (Cell neighbor : myNeighbors) {
             liveNeighborsCount += neighbor.getCurrentState();
         }
 
         // find next state of live cells
-        if (myCell.getCurrentState() == 1) {
+        if (cell.getCurrentState() == 1) {
             if (liveNeighborsCount < 2 || liveNeighborsCount > 3) {
-                myCell.setNextState(0);
+                return 0;
             } else {
-                myCell.setNextState(1);
+                return 1;
             }
-        // find next state of dead cells
-        } else if (myCell.getCurrentState() == 0) {
+            // find next state of dead cells
+        } else if (cell.getCurrentState() == 0) {
             if (liveNeighborsCount == 3) {
-                myCell.setNextState(1);
+                return 1;
             } else {
-                myCell.setNextState(0);
+                return 0;
             }
         }
     }
