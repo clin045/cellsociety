4ec557c13a6f4a90b82ce27f2af9b04ecc2c7565
Christopher Lin <cl349@duke.edu>
2018-09-17 22:56:39 -0400

    Added CellManager and Cell classes. CellManager has a complete initializeGrid() funciton.

---
+++ src/Cell.java
@@ -0,0 +1,42 @@
+/*
+The basic unit of a CA.
+
+@author Christopher Lin cl349
+ */
+public class Cell {
+    private int currentState;
+    private int nextState;
+    private int row;
+    private int col;
+
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public int getCurrentState() {
+        return currentState;
+    }
+
+    public void setCurrentState(int currentState) {
+        this.currentState = currentState;
+    }
+
+    public int getNextState() {
+        return nextState;
+    }
+
+    public void setNextState(int nextState) {
+        this.nextState = nextState;
+    }
+
+    Cell(int row, int col, int initState){
+        this.row = row;
+        this.col = col;
+        this.currentState = initState;
+    }
+}
---
+++ src/CellManager.java
@@ -0,0 +1,27 @@
+
+/*
+This class is responsible for managing the grid and editing cell states
+@author Christopher Lin cl349
+ */
+public class CellManager {
+    private Cell[][] grid;
+
+    //Initializes the grid of cells
+    public void initializeGrid(int rows, int cols, int[][] initConditions){
+        grid = new Cell[rows][cols];
+        for (int i = 0; i < initConditions.length; i ++){
+            for(int j = 0; j < initConditions[0].length; j++){
+                Cell newCell = new Cell(i,j, initConditions[i][j]);
+                grid[i][j] = newCell;
+            }
+        }
+    }
+
+    //detects whether or not a cell is an edge cell
+    private boolean isEdge(Cell cell){
+        return (cell.getRow() == 0 || cell.getRow() == grid.length || cell.getCol() == 0 || cell.getRow() == grid[0].length);
+    }
+
+
+
+}
