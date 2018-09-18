e78131272058a8dfbbbb1974eac33af9530adb9e
Scott McConnell <skm44@duke.edu>
2018-09-18 12:17:40 -0400

    Game of Life Rules Updated

--- src/GameOfLifeRule.java
+++ src/GameOfLifeRule.java
@@ -12,11 +12,20 @@ public class GameOfLifeRule implements RuleInterface{
             liveNeighborsCount += neighbor.getCurrentState();
         }
 
-        if (liveNeighborsCount < 2 || liveNeighborsCount > 3) {
-            myCell.setNextState(0);
-
-        } else {
-            myCell.setNextState(1);
+        // find next state of live cells
+        if (myCell.getCurrentState() == 1) {
+            if (liveNeighborsCount < 2 || liveNeighborsCount > 3) {
+                myCell.setNextState(0);
+            } else {
+                myCell.setNextState(1);
+            }
+        // find next state of dead cells
+        } else if (myCell.getCurrentState() == 0) {
+            if (liveNeighborsCount == 3) {
+                myCell.setNextState(1);
+            } else {
+                myCell.setNextState(0);
+            }
         }
     }
 }
