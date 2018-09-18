812e8cccd9c5d074a1a66fba6e027c924a681528
Scott McConnell <skm44@duke.edu>
2018-09-18 19:12:12 -0400

    RuleInterface changed to int instead of void, refactored to take in Arraylist parameter

--- src/RuleInterface.java
+++ src/RuleInterface.java
@@ -1,11 +1,12 @@
+import java.util.ArrayList;
 
 /**
-** The Rule interface can be extended to create different simulations. Implementing rules as an interface
-provides a flexible system for implementing different kinds of simulations. Rule does not edit cells, it only
- generates the next state; editing cells is the sole responsibility of CellManager.
+ ** The Rule interface can be extended to create different simulations. Implementing rules as an interface
+ provides a flexible system for implementing different kinds of simulations. Rule does not edit cells, it only
+ generates the next state; editing cells is the sole responsibility of main.CellManager.
 
-** @author Scott McConnell skm44
-**/
+ ** @author Scott McConnell skm44
+ **/
 public interface RuleInterface {
-    void applyRule(Cell myCell, Cell[] myNeighbors);
+    int applyRule(Cell myCell, ArrayList<Cell> myNeighbors);
 }
