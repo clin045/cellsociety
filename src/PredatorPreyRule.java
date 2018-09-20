846d51f2ff793c9add41c8dbbce279b47c9f7123
Scott McConnell <skm44@duke.edu>
2018-09-20 18:26:14 -0400

    PredatorPreyRule (unfinished)

---
+++ src/PredatorPreyRule.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+
+/**
+ *
+ * Extension of main.RuleInterface to apply rules specifically for SegregationLife
+ * Returns nextState for myCell
+ *
+ * @author Scott McConnell skm44
+ **/
+
+// states:
+// 0: empty
+// 1: blue  (shark)
+// 2: green (fish)
+public class PredatorPreyRule implements RuleInterface {
+
+    ArrayList<Cell> openNeighbors = new ArrayList<>();
+    ArrayList<Cell> sharkNeighbors = new ArrayList<>();
+    ArrayList<Cell> fishNeighbors = new ArrayList<>();
+
+    public int applyRule(Cell cell, ArrayList<Cell> neighborsArray) {
+        // find states of neighbors
+        for (Cell neighbor : neighborsArray) {
+            if (neighbor.getCurrentState() == 0) {
+                openNeighbors.add(neighbor);
+            } else if (neighbor.getCurrentState() == 1) {
+                sharkNeighbors.add(neighbor);
+            } else if (neighbor.getCurrentState() == 2) {
+                fishNeighbors.add(neighbor);
+            }
+        }
+
+        // find next state of shark cells
+        if (cell.getCurrentState() == 1) {
+            if (fishNeighbors.size() > 0) {
+                return 0;
+            } else if (openNeighbors.size() > 0){
+                return 0;
+            } else {
+                return cell.getCurrentState();
+            }
+        }
+        // find next state of fish cells
+        if (cell.getCurrentState() == 2) {
+            if (openNeighbors.size() > 0) {
+                return 0;
+            } else {
+                return cell.getCurrentState();
+            }
+        }
+    }
+}
