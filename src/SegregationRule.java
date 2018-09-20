4bfb268c26f3dc61c24bb64ae7ad17f5c1a95aff
Scott McConnell <skm44@duke.edu>
2018-09-19 22:23:44 -0400

    SegregationRule logic added (without cell relocation)

--- src/SegregationRule.java
+++ src/SegregationRule.java
@@ -1,4 +1,47 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
-public class SegregationRule {
+/**
+ *
+ * Extension of main.RuleInterface to apply rules specifically for SegregationLife
+ * Returns nextState for myCell
+ *
+ * @author Scott McConnell skm44
+ **/
+
+// states:
+// 0: empty
+// 1: blue
+// 2: red
+public class SegregationRule implements RuleInterface {
+
+    // percent of neighbors that must be similar for happiness
+    private double percentSimilarTolerance;
+
+    // set percentSimilarTolerance based on user input
+    public void setPercentSimilarTolerance(int percent) {
+        percentSimilarTolerance = percent;
+    }
+
+    public int applyRule(Cell cell, ArrayList<Cell> neighborsArray) {
+        int similarNeighborsCount = 0;
+        for (Cell neighbor : neighborsArray) {
+            if (cell.getCurrentState() == neighbor.getCurrentState()) {
+                similarNeighborsCount += neighbor.getCurrentState();
+            }
+        }
+
+        // percent of neighbors that are similar
+        int percentSimilarNeighbors = similarNeighborsCount / neighborsArray.size();
+
+        // find next state of occupied cells
+        if (cell.getCurrentState() == 1 || cell.getCurrentState() == 2) {
+            if (percentSimilarNeighbors < percentSimilarTolerance) {
+                return 0;
+            } else {
+                return cell.getCurrentState();
+            }
+        } else if (cell.getCurrentState() == 0) {
+            return 0;
+        }
+    }
 }
