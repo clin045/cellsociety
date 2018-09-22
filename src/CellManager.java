0f3ebd9d5d774b52ff6512c84d2744897c6ef71b
Christopher Lin <cl349@duke.edu>
2018-09-22 12:59:12 -0400

    Changed initializeGrid to take rule object; getNeighbors now wraps.

--- src/CellManager.java
+++ src/CellManager.java
@@ -12,23 +12,35 @@ public class CellManager {
     RuleInterface myActiveRule;
 
     //Initializes the myGrid of cells
-    public void initializeGrid(int rows, int cols, int[][] initConditions){
+    public void initializeGrid(int rows, int cols, int[][] initConditions, RuleInterface activeRule){
         myRowSize = rows;
         myColSize = cols;
         myGrid = new Cell[rows][cols];
+        myActiveRule = activeRule;
         for (int i = 0; i < initConditions.length; i ++){
             for(int j = 0; j < initConditions[0].length; j++){
                 Cell myNewCell = new Cell(i,j, initConditions[i][j]);
                 myGrid[i][j] = myNewCell;
             }
         }
-        myActiveRule = new GameOfLifeRule();
     }
 
     //detects whether or not a cell is an edge cell
     private boolean isEdge(Cell cell){
-        return (cell.getRow() == myNeighborhoodSize -1 || cell.getRow() == myRowSize -(this.myNeighborhoodSize)
-                || cell.getCol() == myNeighborhoodSize -1 || cell.getCol() == myColSize -(this.myNeighborhoodSize));
+        return (isTop(cell) || isBottom(cell) || isLeft(cell) || isRight(cell));
+    }
+
+    private boolean isTop(Cell cell){
+        return(cell.getRow() == myNeighborhoodSize -1 );
+    }
+    private boolean isBottom(Cell cell){
+        return(cell.getRow() == myRowSize -(this.myNeighborhoodSize));
+    }
+    private boolean isLeft(Cell cell){
+        return(cell.getCol() == myNeighborhoodSize -1);
+    }
+    private boolean isRight(Cell cell){
+        return cell.getCol() == myColSize -(this.myNeighborhoodSize);
     }
 
     public Cell getCell(int row, int col){
@@ -37,16 +49,146 @@ public class CellManager {
 
 
     public ArrayList<Cell> getNeighbors(Cell cell) {
-        if(isEdge(cell)){
-            return null;
-        }
         var myNeighbors = new ArrayList<Cell>();
-        for(int i = cell.getRow()- myNeighborhoodSize; i <= cell.getRow()+ myNeighborhoodSize; i++){
-            for(int j = cell.getCol()- myNeighborhoodSize; j <= cell.getCol()+ myNeighborhoodSize; j++){
-                if(myGrid[i][j] != cell) {
+        int upperBound;
+        int lowerBound;
+        int leftBound;
+        int rightBound;
+
+        if(cell.getRow()-myNeighborhoodSize > 0){
+            upperBound = cell.getRow()-myNeighborhoodSize;
+        }
+        else{
+            upperBound = 0;
+        }
+        if(cell.getRow() + myNeighborhoodSize < myRowSize-1){
+            lowerBound = cell.getRow() + myNeighborhoodSize;
+        }
+        else{
+            lowerBound = myRowSize-1;
+        }
+        if(cell.getCol() - myNeighborhoodSize > 0){
+            leftBound = cell.getCol() - myNeighborhoodSize;
+        }
+        else{
+            leftBound = 0;
+        }
+        if(cell.getCol() + myNeighborhoodSize < myColSize-1){
+            rightBound = cell.getCol() + myNeighborhoodSize;
+        }
+        else{
+            rightBound = myColSize-1;
+        }
+
+        //add all non-wrapping neighbors
+        for(int i = upperBound; i <= lowerBound; i++){
+            for(int j = leftBound; j <= rightBound; j++){
+                if(myGrid[i][j] != cell){
                     myNeighbors.add(myGrid[i][j]);
                 }
             }
+        }
+        //add all wrapping neighbors
+        int upperExceeded = myNeighborhoodSize - cell.getRow();
+        int lowerExceeded = (cell.getRow()+myNeighborhoodSize) - (myRowSize-1);
+        int leftExceeded = myNeighborhoodSize - cell.getCol();
+        int rightExceeded = (cell.getCol()+myNeighborhoodSize) - (myColSize-1);
+
+        if(upperExceeded > 0){
+            for(int i = (myRowSize)-upperExceeded; i <= myRowSize-1; i ++){
+                for(int j = leftBound; j <= rightBound; j++){
+                    if(!myNeighbors.contains(myGrid[i][j]) && myGrid[i][j] != cell){
+                        myNeighbors.add(myGrid[i][j]);
+                    }
+                }
+                if(leftExceeded > 0 ){
+                    for(int j = (myColSize)-leftExceeded; j <= myColSize-1; j ++){
+                        if(!myNeighbors.contains(myGrid[i][j]) && myGrid[i][j] != cell){
+                            myNeighbors.add(myGrid[i][j]);
+                        }
+                    }
+                }
+                if(rightExceeded > 0){
+                    for(int j = 0; j <= rightExceeded-1; j++){
+                        if(!myNeighbors.contains(myGrid[i][j]) && myGrid[i][j] != cell){
+                            myNeighbors.add(myGrid[i][j]);
+                        }
+                    }
+                }
+            }
+        }
+        if(lowerExceeded > 0){
+            for(int i = 0; i <= lowerExceeded-1; i++){
+                for(int j = leftBound; j <= rightBound; j++){
+                    if(!myNeighbors.contains(myGrid[i][j]) && myGrid[i][j] != cell){
+                        myNeighbors.add(myGrid[i][j]);
+                    }
+                }
+                if(leftExceeded > 0 ){
+                    for(int j = (myColSize)-leftExceeded; j <= myColSize-1; j ++){
+                        if(!myNeighbors.contains(myGrid[i][j]) && myGrid[i][j] != cell){
+                            myNeighbors.add(myGrid[i][j]);
+                        }
+                    }
+                }
+                if(rightExceeded > 0){
+                    for(int j = 0; j <= rightExceeded-1; j++){
+                        if(!myNeighbors.contains(myGrid[i][j]) && myGrid[i][j] != cell){
+                            myNeighbors.add(myGrid[i][j]);
+                        }
+                    }
+                }
+            }
+        }
+        if(leftExceeded > 0){
+            for(int j = (myColSize)-leftExceeded; j <= myColSize-1; j ++){
+                for(int i = upperBound; i <= lowerBound; i++){
+                    if(!myNeighbors.contains(myGrid[i][j]) && myGrid[i][j] != cell){
+                        myNeighbors.add(myGrid[i][j]);
+                    }
+                }
+                if(upperExceeded > 0){
+                    for(int i = (myRowSize)-upperExceeded; i <= myRowSize-1; i ++){
+                        if(!myNeighbors.contains(myGrid[i][j]) && myGrid[i][j] != cell){
+                            myNeighbors.add(myGrid[i][j]);
+                        }
+                    }
+                }
+                if(lowerExceeded > 0) {
+                    for (int i = 0; i <= lowerExceeded - 1; i++) {
+                        if (!myNeighbors.contains(myGrid[i][j]) && myGrid[i][j] != cell) {
+                            myNeighbors.add(myGrid[i][j]);
+                        }
+                    }
+                }
+            }
+        }
+        if(rightExceeded > 0){
+            for(int j = 0; j <= rightExceeded-1; j++){
+                for(int i = upperBound; i <= lowerBound; i++){
+                    if(!myNeighbors.contains(myGrid[i][j]) && myGrid[i][j] != cell){
+                        myNeighbors.add(myGrid[i][j]);
+                    }
+                }
+                if(upperExceeded > 0){
+                    for(int i = (myRowSize)-upperExceeded; i <= myRowSize-1; i ++){
+                        if(!myNeighbors.contains(myGrid[i][j]) && myGrid[i][j] != cell){
+                            myNeighbors.add(myGrid[i][j]);
+                        }
+                    }
+                }
+                if(lowerExceeded > 0) {
+                    for (int i = 0; i <= lowerExceeded - 1; i++) {
+                        if (!myNeighbors.contains(myGrid[i][j]) && myGrid[i][j] != cell) {
+                            myNeighbors.add(myGrid[i][j]);
+                        }
+                    }
+                }
+            }
+        }
+        //wrap corners
+        if(upperExceeded > 0 && rightExceeded > 0){
+
         }
         return myNeighbors;
     }
