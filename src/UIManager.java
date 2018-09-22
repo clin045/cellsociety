839da3b43b134fa906f78bed5141db5f537919e0
Allen Qiu <asq3@duke.edu>
2018-09-22 13:16:21 -0400

    Updated UI

--- src/UIManager.java
+++ src/UIManager.java
@@ -1,9 +1,8 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 import javafx.scene.control.Button;
@@ -11,33 +10,36 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.util.ResourceBundle;
 
 
 public class UIManager extends Application {
+    private static final String DEFAULT_RESOURCE_PACKAGE = "English";
+    private ResourceBundle myResources = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE);
+
     public void start(Stage stage){
         //create text for XML label
-        Text label1 = new Text("Choose a XML file:");
 
-        Text label2 = new Text("No file chosen.");
+        Text label1 = new Text(myResources.getString("ChooseFileLabel"));
+
+        Text label2 = new Text(myResources.getString("NoFile"));
 
         //setup load button
-        Button load = new Button("Select...");
-        load.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                //load file chooser
-                FileChooser myFileChooser = new FileChooser();
-                myFileChooser.setTitle("Choose XML file");
-                FileChooser.ExtensionFilter xmlFilter = new FileChooser.ExtensionFilter("XML files (*.xml)", "*.xml");
-                myFileChooser.getExtensionFilters().add(xmlFilter);
-                File chosen = myFileChooser.showOpenDialog(stage);
-                if(chosen != null){
-                    label2.setText(chosen.getName());
-                }
+        Button load = new Button(myResources.getString("SelectButton"));
+        load.setOnAction(event -> {
+            //load file chooser
+            FileChooser myFileChooser = new FileChooser();
+            myFileChooser.setTitle(myResources.getString("ChooserWindowTitle"));
+            FileChooser.ExtensionFilter xmlFilter = new FileChooser.ExtensionFilter("XML files (*.xml)", "*.xml");
+            myFileChooser.getExtensionFilters().add(xmlFilter);
+            File chosen = myFileChooser.showOpenDialog(stage);
+            if(chosen != null){
+                label2.setText(chosen.getName());
             }
         });
 
-        Button starter = new Button("Start!");
+        Button starter = new Button(myResources.getString("StartButton"));
+        starter.setOnAction(event -> createSimulator(stage));
 
         //create a gridpane
         GridPane myGridPane = new GridPane();
@@ -55,12 +57,52 @@ public class UIManager extends Application {
         //create scene
         Scene myScene = new Scene(myGridPane);
 
-        stage.setTitle("CA Simulator");
+        stage.setTitle(myResources.getString("WindowTitle"));
 
         stage.setScene(myScene);
 
         stage.show();
     }
+
+    private void createSimulator(Stage stage){
+        GridPane rootPane = new GridPane();
+        rootPane.setPadding(new Insets(10, 10, 10, 10));
+        rootPane.setMinSize(400, 400);
+        rootPane.setAlignment(Pos.CENTER);
+
+        GridPane simulatorGridPane = new GridPane();
+        simulatorGridPane.setGridLinesVisible(true);
+        simulatorGridPane.setAlignment(Pos.CENTER);
+        for(int i=0;i<16;i++){
+            for(int j=0;j<16;j++){
+                simulatorGridPane.add(new Text("Test"), i, j);
+            }
+        }
+        FlowPane controls = new FlowPane();
+        controls.setPadding(new Insets(10, 10, 10, 10));
+        controls.setAlignment(Pos.CENTER);
+        Button play = new Button(myResources.getString("PlayButton"));
+        Button pause = new Button(myResources.getString("PauseButton"));
+        Button step = new Button(myResources.getString("StepButton"));
+        Button halfSpeed = new Button(myResources.getString("HalfSpeedButton"));
+        Button normalSpeed = new Button(myResources.getString("NormalSpeedButton"));
+        Button doubleSpeed = new Button(myResources.getString("DoubleSpeedButton"));
+        Button newSimulation = new Button(myResources.getString("NewSimulation"));
+        controls.getChildren().add(play);
+        controls.getChildren().add(pause);
+        controls.getChildren().add(step);
+        controls.getChildren().add(halfSpeed);
+        controls.getChildren().add(normalSpeed);
+        controls.getChildren().add(doubleSpeed);
+        controls.getChildren().add(newSimulation);
+
+        rootPane.add(simulatorGridPane, 0, 0);
+        rootPane.add(controls, 0, 1);
+
+        Scene simulatorScene = new Scene(rootPane);
+        stage.setScene(simulatorScene);
+    }
+
     public static void main(String args[]){
         launch(args);
     }
