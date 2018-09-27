21b44a3087fdfa93ab79162c3353eccea569de12
Allen Qiu <asq3@duke.edu>
2018-09-27 16:36:09 -0400

    Refactor GUI

--- src/UIManager.java
+++ src/UIManager.java
@@ -1,7 +1,6 @@
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -9,7 +8,6 @@ import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
@@ -17,6 +15,7 @@ import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+
 import model.*;
 import xml.Simulation;
 import xml.XMLParser;
@@ -26,11 +25,14 @@ import java.util.ResourceBundle;
 
 /**
  * This class manages the UI and interacts with the XML parser and the cell manager.
- *
  * @author Allen Qiu (asq3)
  */
 public class UIManager extends Application {
     private static final double MILLISECOND_DELAY = 300;
+    private static final int WINDOW_SIZE = 600;
+    private static final int PADDING_SIZE = 10;
+    private static final int HORIZONTAL_GUI_GAP = 5;
+    private static final int VERTICAAL_GUI_GAP = 5;
     private static final String DEFAULT_RESOURCE_PACKAGE = "English";
     private ResourceBundle myResources = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE);
     private File chosen;
@@ -38,15 +40,14 @@ public class UIManager extends Application {
     private GridPane simulatorGridPane;
     private int rows;
     private int columns;
+    private String title;
+    private String author;
+    private String simulationName;
     private String[] colors;
     private Timeline animation = new Timeline();
     private Stage myStage;
 
-    public static void main(String args[]) {
-        launch(args);
-    }
-
-    public void start(Stage stage) {
+    public void start(Stage stage){
         myStage = stage;
 
         Text chooseFileLabel = new Text(myResources.getString("ChooseFileLabel"));
@@ -60,7 +61,7 @@ public class UIManager extends Application {
             myFileChooser.getExtensionFilters().add(xmlFilter);
             myFileChooser.setInitialDirectory(new File(System.getProperty("user.dir")));
             chosen = myFileChooser.showOpenDialog(myStage);
-            if (chosen != null) {
+            if(chosen != null){
                 fileName.setText(chosen.getName());
             }
         });
@@ -69,13 +70,13 @@ public class UIManager extends Application {
         startButton.setOnAction(event -> createSimulator());
 
         GridPane myGridPane = new GridPane();
-        myGridPane.setMinSize(600, 600);
-        myGridPane.setPadding(new Insets(10, 10, 10, 10));
-        myGridPane.setVgap(5);
-        myGridPane.setHgap(5);
+        myGridPane.setMinSize(WINDOW_SIZE, WINDOW_SIZE);
+        myGridPane.setPadding(new Insets(PADDING_SIZE, PADDING_SIZE, PADDING_SIZE, PADDING_SIZE));
+        myGridPane.setVgap(VERTICAAL_GUI_GAP);
+        myGridPane.setHgap(HORIZONTAL_GUI_GAP);
         myGridPane.setAlignment(Pos.CENTER);
 
-        myGridPane.add(chooseFileLabel, 0, 0);
+        myGridPane.add(chooseFileLabel, 0,0);
         myGridPane.add(fileName, 0, 1);
         myGridPane.add(loadButton, 1, 1);
         myGridPane.add(startButton, 0, 2);
@@ -87,7 +88,7 @@ public class UIManager extends Application {
         myStage.show();
     }
 
-    private void createSimulator() {
+    private void createSimulator(){
         initializeWindow();
 
         var frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY), e -> step());
@@ -97,77 +98,86 @@ public class UIManager extends Application {
 
     }
 
-    private void initializeWindow() {
-        Simulation configs = new XMLParser("media").getSimulation(chosen);
-        rows = configs.getRows();
-        columns = configs.getCols();
-        int[][] initialStates = configs.getConfigs();
-        colors = configs.getColors().split(",");
+    private void initializeWindow(){
+        int[][] initialStates = readConfiguration();
 
         GridPane rootPane = new GridPane();
-        rootPane.setPadding(new Insets(10, 10, 10, 10));
-        rootPane.setMinSize(600, 600);
+        rootPane.setPadding(new Insets(PADDING_SIZE, PADDING_SIZE, PADDING_SIZE, PADDING_SIZE));
+        rootPane.setMinSize(WINDOW_SIZE, WINDOW_SIZE);
         rootPane.setAlignment(Pos.CENTER);
 
-        GridPane titleBlock = createTitleBlock(configs);
         createAnimationBlock(initialStates);
-        FlowPane controls = createControlsBlock();
 
-        rootPane.add(titleBlock, 0, 0);
+        rootPane.add(createTitleBlock(), 0, 0);
         rootPane.add(simulatorGridPane, 0, 1);
-        rootPane.add(controls, 0, 2);
+        rootPane.add(createControlsBlock(), 0, 2);
 
-        Rule myRule = findSimulationType(configs.getSimulationName());
+        Rule myRule = findSimulationType(simulationName);
         myCellManager = new CellManager(rows, columns, initialStates, myRule, CellManager.SQUARE_GRID);
 
         myStage.setScene(new Scene(rootPane));
     }
 
-    private Rule findSimulationType(String name) {
+    private int[][] readConfiguration(){
+        Simulation configs = new XMLParser("media").getSimulation(chosen);
+        rows = configs.getRows();
+        columns = configs.getCols();
+        title = configs.getTitle();
+        author = configs.getAuthor();
+        simulationName = configs.getSimulationName();
+        int[][] initialStates = configs.getConfigs();
+        colors = configs.getColors().split(",");
+        return initialStates;
+    }
+
+    private Rule findSimulationType(String name){
         Rule myRule;
-        if (name.compareToIgnoreCase("Game of Life") == 0) {
+        if(name.compareToIgnoreCase("Game of Life") == 0){
             myRule = new GameOfLifeRule();
-        } else if (name.compareToIgnoreCase("Predator Prey") == 0) {
+        }
+        else if(name.compareToIgnoreCase("Predator Prey") == 0){
             myRule = new PredatorPreyRule();
-        } else if (name.compareToIgnoreCase("Fire") == 0) {
+        }
+        else if(name.compareToIgnoreCase("Fire") == 0){
             myRule = new FireRule();
-        } else {
+        }
+        else {
             myRule = new SegregationRule();
         }
         return myRule;
     }
 
-    private GridPane createTitleBlock(Simulation configs) {
-        Label title = new Label(configs.getTitle());
-        Label author = new Label(configs.getAuthor());
-        Label simulationName = new Label(configs.getSimulationName());
+    private GridPane createTitleBlock(){
+        Label myTitle = new Label(title);
+        Label myAuthor = new Label(author);
+        Label mySimulationName = new Label(simulationName);
 
         GridPane displayInfo = new GridPane();
         displayInfo.setAlignment(Pos.CENTER);
-        displayInfo.setPadding(new Insets(10, 10, 10, 10));
-        displayInfo.add(title, 0, 0);
-        GridPane.setHalignment(title, HPos.CENTER);
-        displayInfo.add(author, 0, 1);
-        GridPane.setHalignment(author, HPos.CENTER);
-        displayInfo.add(simulationName, 0, 2);
-        GridPane.setHalignment(simulationName, HPos.CENTER);
+        displayInfo.setPadding(new Insets(PADDING_SIZE, PADDING_SIZE, PADDING_SIZE, PADDING_SIZE));
+        displayInfo.add(myTitle, 0, 0);
+        GridPane.setHalignment(myTitle, HPos.CENTER);
+        displayInfo.add(myAuthor, 0, 1);
+        GridPane.setHalignment(myAuthor, HPos.CENTER);
+        displayInfo.add(mySimulationName, 0, 2);
+        GridPane.setHalignment(mySimulationName, HPos.CENTER);
 
         return displayInfo;
     }
 
-    private void createAnimationBlock(int[][] initialStates) {
+    private void createAnimationBlock(int[][] initialStates){
         simulatorGridPane = new GridPane();
         simulatorGridPane.setAlignment(Pos.CENTER);
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<columns;j++){
                 simulatorGridPane.add(createCell(colors[initialStates[i][j]], rows, columns), i, j);
             }
         }
     }
 
-    private FlowPane createControlsBlock() {
+    private FlowPane createControlsBlock(){
         FlowPane controls = new FlowPane();
-        controls.setPadding(new Insets(10, 10, 10, 10));
+        controls.setPadding(new Insets(PADDING_SIZE, PADDING_SIZE, PADDING_SIZE, PADDING_SIZE));
         controls.setAlignment(Pos.CENTER);
 
         Button play = new Button(myResources.getString("PlayButton"));
@@ -219,10 +229,10 @@ public class UIManager extends Application {
         return null;
     }
 
-    private void step() {
+    private void step(){
         myCellManager.nextGeneration();
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<columns;j++){
                 BorderPane thisCellPane = (BorderPane) getNodeFromGridPane(i, j);
                 Cell thisCell = myCellManager.getGrid().getCell(i, j);
                 if (thisCellPane != null) {
@@ -232,41 +242,38 @@ public class UIManager extends Application {
         }
     }
 
-    private void updateCellAppearance(String color, BorderPane myCell) {
+    private void updateCellAppearance(String color, BorderPane myCell){
         myCell.setStyle("-fx-border-color: #000000;" +
                 "-fx-background-color: #" + color + ";" +
                 "-fx-border-width: 1;");
     }
 
-    private BorderPane createCell(String color, int rows, int columns) {
+    private BorderPane createCell(String color, int rows, int columns){
         BorderPane cell = new BorderPane();
-        int cellSize = 500 / Math.max(rows, columns);
+        int cellSize = (WINDOW_SIZE-100)/Math.max(rows, columns);
         cell.setMinSize(cellSize, cellSize);
         cell.setStyle("-fx-border-color: #000000;" +
                 "-fx-background-color: #" + color + ";" +
                 "-fx-border-width: 1;");
-        cell.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                toggleNextState(cell);
-            }
-        });
+        cell.setOnMouseClicked(event -> toggleNextState(cell));
         return cell;
     }
 
-    private void toggleNextState(BorderPane cell) {
+    private void toggleNextState(BorderPane cell){
         int numStates = colors.length;
-        System.out.println(numStates);
         Cell thisCell = myCellManager.getGrid().getCell(GridPane.getColumnIndex(cell), GridPane.getRowIndex(cell));
-        System.out.println(thisCell.getCurrentState());
-        if (thisCell.getCurrentState() == numStates - 1) {
-            //recircle
+        if(thisCell.getCurrentState() ==  numStates-1){
             thisCell.setCurrentState(0);
             thisCell.setNextState(0);
-        } else {
-            thisCell.setNextState(thisCell.getCurrentState() + 1);
-            thisCell.setCurrentState(thisCell.getCurrentState() + 1);
+        }
+        else {
+            thisCell.setNextState(thisCell.getCurrentState()+1);
+            thisCell.setCurrentState(thisCell.getCurrentState()+1);
         }
         updateCellAppearance(colors[thisCell.getNextState()], cell);
+    }
+
+    public static void main(String args[]){
+        launch(args);
     }
 }
