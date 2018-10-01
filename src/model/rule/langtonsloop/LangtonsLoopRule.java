7898a8ae2cc0c4e448c3e0a66af27cc8cefa9a50
Scott McConnell <skm44@duke.edu>
2018-10-01 00:11:42 -0400

    LangtonRule added

---
+++ src/model/rule/langtonsloop/LangtonsLoopRule.java
@@ -0,0 +1,55 @@
+package model.rule.langtonsloop;
+
+
+import model.Cell;
+import model.rule.Rule;
+
+import java.util.List;
+
+/**
+ * Extension of main.model.rule.Rule to apply rules specifically for Langton's Loop
+ * Returns nextState for myCell
+ *
+ * @author Scott McConnell skm44
+ **/
+public class LangtonsLoopRule extends Rule {
+    static final int NUM_PASSES = 1;
+    static final int VACANT = 0; //black
+    static final int SIGNAL = 1; //dark blue
+    static final int SHEATH = 2; //red
+    static final int VACANT = 0;
+    static final int TURN = 4; //green
+    static final int MESSENGER = 5; //pink
+    static final int VACANT = 0;
+    static final int VACANT = 0;
+    static final int ADVANCE = 7; //light blue
+
+
+    static final int EAST = 0;
+    static final int NORTH = 1;
+    static final int WEST = 2;
+    static final int SOUTH = 4;
+
+    private int myDirection = EAST;
+
+    public LangtonsLoopRule(){
+        myNumStates = 8;
+    }
+
+    public int getPasses() {
+        return NUM_PASSES;
+    }
+
+    public int getNeighborhoodSize() {
+        return 1;
+    }
+
+    @Override
+    public Class getCellType() {
+        return Cell.class;
+    }
+
+    public void applyRule(Cell cell, List<Cell> neighborsArray, int passNum) {
+
+    }
+}
